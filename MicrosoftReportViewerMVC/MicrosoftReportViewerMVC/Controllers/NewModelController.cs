862d6f3df8183bd937f86eb91f35e1ba622c5e2f
sanshanthan <sanshanthan@yahoo.com>
2019-07-24 00:28:15 +0530

    Table Data Int

---
+++ MicrosoftReportViewerMVC/MicrosoftReportViewerMVC/Controllers/NewModelController.cs
@@ -0,0 +1,23 @@
+﻿using MicrosoftReportViewerMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MicrosoftReportViewerMVC.Controllers
+{
+    public class NewModelController : Controller
+    {
+        ABC_Company_DBEntities dbcon = new ABC_Company_DBEntities();
+        // GET: NewModel
+        public ActionResult StateArea()
+        {
+            using (dbcon)
+            {
+                var v = dbcon.OutstandingAndActiveProjects.ToList();
+                return View(v);
+            }
+        }
+    }
+}
